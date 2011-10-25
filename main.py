6700ae3c30ef11142d3a6965ad4e118b4bbfc3b0
Tom Gross <tom@toms-projekte.de>
2011-10-25 06:38:34 +0200

    added spelling and player logic

--- main.py
+++ main.py
@@ -14,18 +14,75 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
+import urllib2
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
+from google.appengine.ext.webapp import template
 
-from spellingtable import spellingtable
+from spellingtable import spellingtable, dialects
 
 class MainHandler(webapp.RequestHandler):
+
+    template = os.path.join(os.path.dirname(__file__), 'templates', 'index.html')
+
     def get(self):
-        self.response.out.write('Hello world!')
+        template_values = {'dialects': dialects}
+        self.response.out.write(template.render(self.template, template_values))
+
+
+def getspelling(q, dialect=5):
+    dialecttable = spellingtable[int(dialect)]
+    return [dialecttable.get(char.upper(), '') for char in q if char.strip()]
+
+class SpellHandler(webapp.RequestHandler):
+
+    template = os.path.join(os.path.dirname(__file__), 'templates', 'index.html')
+
+    def post(self):
+        q = self.request.get('q', '')
+        dialect = self.request.get('dialect', 5)
+        lang = self.request.get('lang', 'de')
+
+        spelling_list = getspelling(q, dialect)
+
+        words = '+'.join(spelling_list)
+        audio_url = '/play?lang=%s&q=%s&dialect=%s' % (lang, words, dialect)
+        template_values = {'dialects': dialects,
+                           'spelling_list': spelling_list,
+                           'active_dialect': dialect,
+                           'audio_url': audio_url}
+        self.response.out.write(template.render(self.template, template_values))
+
+    get = post
+
+class AudioHandler(webapp.RequestHandler):
+
+    def get(self):
+        q = self.request.get('q', 'awesome')
+        dialect = self.request.get('dialect', 5)
+        lang = self.request.get('lang', 'de')
+
+        words = '+'.join(getspelling(q, dialect))
+        ua = self.request.headers.get('User-Agent'
+            "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11")
+
+        url = 'http://translate.google.com/translate_tts?tl=%s&q=%s' % (lang, words)
+        headers = {'User-Agent': ua}
+        req = urllib2.Request(url, headers=headers)
+
+        r = urllib2.urlopen(req)
+        c = r.read()
+        r.close()
+        self.response.headers.add_header("Content-Type", "audio/mpeg3")
+        self.response.out.write(c)
 
 
 def main():
-    application = webapp.WSGIApplication([('/', MainHandler)],
+    application = webapp.WSGIApplication([('/', MainHandler),
+                                          ('/spell', SpellHandler),
+                                          ('/play', AudioHandler)],
                                          debug=True)
     util.run_wsgi_app(application)
 
